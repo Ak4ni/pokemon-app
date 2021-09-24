8f23946ec49ddbaadd8a5eacc76d3732122cec15
Andres Rivera Perez <89215774+Ak4ni@users.noreply.github.com>
2021-09-23 20:12:09 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
-alert('hello world!');
-let favoriteFood = hamburgers;
-document.write(favoriteFood)
+//Poekedex application
+
+//pokenmonList Variable
+
+let pokemonList= [
+{name: 'charizard', height: 2, type: [ 'fire', 'flying']},
+{name: 'venusaur', height: 2, type: ['grass', 'poison']},
+{name: 'balstoise', height: 2, type: ['water']},
+{name: 'electabuzz', height: 1, type: ['electric']},
+];

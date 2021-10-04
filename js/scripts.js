9c9b5d5ebcad8e300b6b3993501a11d2eda7f20e
Andres Rivera Perez <89215774+Ak4ni@users.noreply.github.com>
2021-10-04 17:11:08 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,24 +3,25 @@
 //pokenmonList Variable
 
 let pokemonList = [
-{name: 'charizard', height: 2, type: [ 'fire', 'flying']},
-{name: 'venusaur', height: 2, type: ['grass', 'poison']},
-{name: 'balstoise', height: 2, type: ['water']},
-{name: 'electabuzz', height: 1, type: ['electric']},
+ {name: 'charizard', height: 2, type: [ 'fire', 'flying']},
+ {name: 'venusaur', height: 2, type: ['grass', 'poison']},
+ {name: 'balstoise', height: 2, type: ['water']},
+ {name: 'electabuzz', height: 1, type: ['electric']},
 ];
 
 
 
 const maxHeight = 2;
+
 for (let i = 0; i < pokemonList.length; i++) {
-let pokemonObject = pokemonList[i];
-console.log(${pokemonObject.name}-${pokemonObject.height});
-document.write(<p>${pokemonObject.name}</p>);
+  let pokemonObject = pokemonList[i];
+    console.log(`${pokemonObject.name}-${pokemonObject.height}`);
+      document.write(`<p>${pokemonObject.name}</p>`);
 
 if ( pokemonObject.height >= maxHeight ) {
-   document.write(<p>${pokemonObject.name} - Wow, that’s big!</p>);
+   document.write(`<p>${pokemonObject.name} - Wow, that’s big!</p>`);
 } else {
-    document.write(<p>${pokemonObject.name} - Wow, that’s big!</p>);
+    document.write(`<p>${pokemonObject.name} - Thats a Pokemon!</p>`);
  }
 }
  
